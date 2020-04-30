84829321a6a349025403ae4a4a18cd87d8182269
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-04-30 00:34:32 -0300

    DTO Concept, TS Omit Helper

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,7 +6,11 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {});
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -23,7 +27,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ error: 'This appointment is already booked.' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
