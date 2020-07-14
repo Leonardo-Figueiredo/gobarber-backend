65f2333c3741a761b1205b8152d4b0a3d6a63df9
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-07-13 22:36:36 -0300

    Learning tests with jest.

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,11 +1,19 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment.', () => {
-    expect(1 + 2).toBe(3);
-  });
+  it('should be able to create a new appointment.', async () => {
+    const appointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      appointmentRepository,
+    );
 
-  // it('should be not able to create two appointment on the same time.', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
 });
