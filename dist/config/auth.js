ab6ffe90f5184927727629aaaf82911ca6b80bf3
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-08-16 16:43:54 -0300

    Deleting Dist paste.

--- dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: '2cd062c49542cdfc9321fb360be83a1b',
-        expiresIn: '1d',
-    },
-};
